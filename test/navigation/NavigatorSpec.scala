619e018b6c31395fb41aa89c15b0dd2b994e505b
“JoshuaH01” <joshua.hill@digital.hmrc.gov.uk>
2020-02-05 12:28:18 +0000

    RemoveIndex| Removed index route, controller, view and specs

--- test/navigation/NavigatorSpec.scala
+++ test/navigation/NavigatorSpec.scala
@@ -29,12 +29,6 @@ class NavigatorSpec extends SpecBase {
 
     "in Normal mode" must {
 
-      "go to Index from a page that doesn't exist in the route map" in {
-
-        case object UnknownPage extends Page
-        navigator.nextPage(UnknownPage, NormalMode, UserAnswers("id")) mustBe routes.IndexController.onPageLoad()
-      }
-
       "go to BeforeYouContinue from IsAgentManagingTrust" in {
 
         navigator.nextPage(IsAgentManagingTrustPage, NormalMode, UserAnswers("id")) mustBe routes.BeforeYouContinueController.onPageLoad()
