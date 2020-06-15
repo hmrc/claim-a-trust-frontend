bf5bfa44ff655864b518a729735ac2603b89effa
Andrew Armstrong <35451916+andrew-armstrong80@users.noreply.github.com>
2020-06-15 14:01:32 +0100

    TRUS-2692 : Error message prefix added and tests updated

--- test/views/behaviours/StringViewBehaviours.scala
+++ test/views/behaviours/StringViewBehaviours.scala
@@ -68,8 +68,7 @@ trait StringViewBehaviours extends QuestionViewBehaviours[String] {
 
           val doc = asDocument(createView(form.withError(error)))
           val errorSpan = doc.getElementsByClass("error-message").first
-          errorSpan.text mustBe messages(errorMessage)
-        }
+          errorSpan.text mustBe s"""${messages("site.error")} ${messages(errorMessage)}"""        }
 
         "show an error prefix in the browser title" in {
 
