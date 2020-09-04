ed24aa9e926f8a6685b637d8ef89b71d2c6039f7
“JoshuaH01” <34240122+JoshuaH01@users.noreply.github.com>
2020-09-04 15:23:55 +0100

    TRUS-2851| Upgraded dependencies and plugins

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,25 +5,25 @@ object AppDependencies {
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "org.reactivemongo" %% "play2-reactivemongo"            % "0.18.8-play26",
-    "uk.gov.hmrc"       %% "logback-json-logger"            % "3.1.0",
-    "uk.gov.hmrc"       %% "govuk-template"                 % "5.36.0-play-26",
+    "org.reactivemongo" %% "play2-reactivemongo"            % "0.20.11-play26",
+    "uk.gov.hmrc"       %% "logback-json-logger"            % "4.6.0",
+    "uk.gov.hmrc"       %% "govuk-template"                 % "5.52.0-play-26",
     "uk.gov.hmrc"       %% "play-health"                    % "3.14.0-play-26",
-    "uk.gov.hmrc"       %% "play-ui"                        % "8.2.0-play-26",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "0.2.0",
-    "uk.gov.hmrc"       %% "bootstrap-play-26"              % "1.1.0",
-    "uk.gov.hmrc"       %% "play-whitelist-filter"          % "2.0.0"
+    "uk.gov.hmrc"       %% "play-ui"                        % "8.8.0-play-26",
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.2.0-play-26",
+    "uk.gov.hmrc"       %% "bootstrap-play-26"              % "1.14.0",
+    "uk.gov.hmrc"       %% "play-whitelist-filter"          % "3.1.0-play-26"
   )
 
   val test = Seq(
-    "org.scalatest"               %% "scalatest"          % "3.0.7",
+    "org.scalatest"               %% "scalatest"          % "3.0.8",
     "org.scalatestplus.play"      %% "scalatestplus-play" % "3.1.2",
     "org.pegdown"                 %  "pegdown"            % "1.6.0",
     "org.jsoup"                   %  "jsoup"              % "1.10.3",
     "com.typesafe.play"           %% "play-test"          % PlayVersion.current,
     "org.mockito"                 %  "mockito-all"        % "1.10.19",
     "org.scalacheck"              %% "scalacheck"         % "1.14.0",
-    "com.github.tomakehurst"      % "wiremock-standalone"      % "2.17.0"
+    "com.github.tomakehurst"      % "wiremock-standalone"      % "2.25.1"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
