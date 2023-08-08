27b91d05e8896c7709cccc89bf7d405aedd062f4
stephenheath101 <98922374+stephenheath101@users.noreply.github.com>
2023-08-08 14:37:59 +0100

    WIP

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,12 +3,12 @@ import sbt._
 object AppDependencies {
   import play.core.PlayVersion
 
-  val bootstrapFrontendPlay28 = "7.19.0"
+  val bootstrapFrontendPlay28 = "7.20.0"
 
   private val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"             % "0.74.0",
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "7.14.0-play-28",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"             % "1.3.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "7.16.0-play-28",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.13.0-play-28",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"     % bootstrapFrontendPlay28,
     "org.typelevel"     %% "cats-core"                      % "2.9.0"
@@ -17,7 +17,7 @@ object AppDependencies {
   private val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                 %% "bootstrap-test-play-28"   % bootstrapFrontendPlay28,
     "org.scalatest"               %% "scalatest"                % "3.2.16",
-    "uk.gov.hmrc.mongo"           %% "hmrc-mongo-test-play-28"  % "0.74.0",
+    "uk.gov.hmrc.mongo"           %% "hmrc-mongo-test-play-28"  % "1.3.0",
     "org.scalatestplus.play"      %% "scalatestplus-play"       % "5.1.0",
     "org.scalatestplus"           %% "scalatestplus-scalacheck" % "3.1.0.0-RC2",
     "org.pegdown"                 %  "pegdown"                  % "1.6.0",
