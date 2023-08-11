f13f9d79fe433bda50ff76eb5bbefdae38a52eb4
Stephen <98922374+stephenheath101@users.noreply.github.com>
2023-08-11 15:59:36 +0100

    Ddce-4440: Mongo upgrade to v1.3.0 (#138)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,12 +3,12 @@ import sbt._
 object AppDependencies {
   import play.core.PlayVersion
 
-  val bootstrapFrontendPlay28 = "7.19.0"
+  val bootstrapFrontendPlay28 = "7.20.0"
 
   private val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"             % "0.74.0",
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "7.14.0-play-28",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"             % "1.3.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "7.16.0-play-28",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.13.0-play-28",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"     % bootstrapFrontendPlay28,
     "org.typelevel"     %% "cats-core"                      % "2.9.0"
@@ -17,14 +17,13 @@ object AppDependencies {
   private val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                 %% "bootstrap-test-play-28"   % bootstrapFrontendPlay28,
     "org.scalatest"               %% "scalatest"                % "3.2.16",
-    "uk.gov.hmrc.mongo"           %% "hmrc-mongo-test-play-28"  % "0.74.0",
-    "org.scalatestplus.play"      %% "scalatestplus-play"       % "5.1.0",
-    "org.scalatestplus"           %% "scalatestplus-scalacheck" % "3.1.0.0-RC2",
+    "uk.gov.hmrc.mongo"           %% "hmrc-mongo-test-play-28"  % "1.3.0",
+    "org.scalatestplus"           %% "mockito-4-11"             % "3.2.16.0",
+    "org.scalatestplus"           %% "scalacheck-1-17"          % "3.2.16.0",
     "org.pegdown"                 %  "pegdown"                  % "1.6.0",
     "org.jsoup"                   %  "jsoup"                    % "1.16.1",
-    "com.typesafe.play"           %% "play-test"                % PlayVersion.current,
-    "org.mockito"                 %% "mockito-scala-scalatest"  % "1.17.14",
     "org.scalacheck"              %% "scalacheck"               % "1.17.0",
+    "org.mockito"                 %% "mockito-scala-scalatest"  % "1.17.14",
     "com.github.tomakehurst"      % "wiremock-standalone"       % "2.27.2",
     "com.vladsch.flexmark"        %  "flexmark-all"             % "0.64.8"
   ).map(_ % Test)
