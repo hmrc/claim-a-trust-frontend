5ac243139179d52b04f44f272d5a4d8a545a5ece
Mark Beresford <mark.beresford@digital.hmrc.gov.uk>
2023-08-22 10:55:15 +0100

    DDCE-4194: Update CSP and update deps

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,17 +1,15 @@
 import sbt._
 
 object AppDependencies {
-  import play.core.PlayVersion
 
-  val bootstrapFrontendPlay28 = "7.20.0"
+  val bootstrapFrontendPlay28 = "7.21.0"
 
   private val compile: Seq[ModuleID] = Seq(
-    play.sbt.PlayImport.ws,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"             % "1.3.0",
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "7.16.0-play-28",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "7.19.0-play-28",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.13.0-play-28",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"     % bootstrapFrontendPlay28,
-    "org.typelevel"     %% "cats-core"                      % "2.9.0"
+    "org.typelevel"     %% "cats-core"                      % "2.10.0"
   )
 
   private val test: Seq[ModuleID] = Seq(
@@ -23,8 +21,8 @@ object AppDependencies {
     "org.pegdown"                 %  "pegdown"                  % "1.6.0",
     "org.jsoup"                   %  "jsoup"                    % "1.16.1",
     "org.scalacheck"              %% "scalacheck"               % "1.17.0",
-    "org.mockito"                 %% "mockito-scala-scalatest"  % "1.17.14",
-    "com.github.tomakehurst"      % "wiremock-standalone"       % "2.27.2",
+    "org.mockito"                 %% "mockito-scala-scalatest"  % "1.17.22",
+    "org.wiremock"                %  "wiremock-standalone"      % "3.0.0",
     "com.vladsch.flexmark"        %  "flexmark-all"             % "0.64.8"
   ).map(_ % Test)
 
