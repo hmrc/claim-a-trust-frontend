421c7f4c20645adce582cf289a42fe221d9d66e8
matthew-gibson-18 <175828706+matthew-gibson-18@users.noreply.github.com>
2024-08-27 12:05:24 +0100

    DDCE-5602 Tiding up

--- app/controllers/BeforeYouContinueController.scala
+++ app/controllers/BeforeYouContinueController.scala
@@ -68,7 +68,8 @@ class BeforeYouContinueController @Inject()(
   }
 
   def onSubmit: Action[AnyContent] = actions.authWithData.async { implicit request =>
-      val result = for {
+
+    val result = for {
         identifier <- TrustEnvelope.fromOption(request.userAnswers.get(IdentifierPage))
         isManagedByAgent <- TrustEnvelope.fromOption(request.userAnswers.get(IsAgentManagingTrustPage))
         relationshipStatus <- relationship.check(request.internalId, identifier)
