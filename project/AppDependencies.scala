a872bbf2c31a5a818054ca7fe7e523cf95541e58
mithra-balakrishnan <177737570+mithra-balakrishnan@users.noreply.github.com>
2025-06-23 09:18:26 +0100

    DDCE-3621 - dependency updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,22 +1,22 @@
 import sbt.*
 
 object AppDependencies {
-  val bootstrapVersion = "9.5.0"
-  val mongoVersion = "2.3.0"
+  val bootstrapVersion = "9.13.0"
+  val mongoVersion = "2.6.0"
 
   private val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % mongoVersion,
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % bootstrapVersion,
     "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "11.7.0",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.2.0",
-    "org.typelevel"     %% "cats-core"                             % "2.12.0"
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.3.0",
+    "org.typelevel"     %% "cats-core"                             % "2.13.0"
   )
 
   private val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                 %% "bootstrap-test-play-30"   % bootstrapVersion,
     "uk.gov.hmrc.mongo"           %% "hmrc-mongo-test-play-30"  % mongoVersion,
     "org.scalatestplus"           %% "scalacheck-1-17"          % "3.2.18.0",
-    "org.jsoup"                   %  "jsoup"                    % "1.18.3",
+    "org.jsoup"                   %  "jsoup"                    % "1.21.1",
     "org.scalacheck"              %% "scalacheck"               % "1.18.1"
   ).map(_ % Test)
 
